935fff822d97942c45b673b212c2e51a0a36ae15
Riya Bansal <35702912+riyabansal98@users.noreply.github.com>
2023-06-01 00:46:19 +0530

    Create FirstMissingPositive.java

---
+++ L1/Arrays and Hashing - Part 1/Python/FirstMissingPositive.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Base case.
+        int contains = 0;
+        for (int i = 0; i < n; i++)
+            if (nums[i] == 1) {
+                contains++;
+                break;
+            }
+
+        if (contains == 0)
+            return 1;
+
+        // Replace negative numbers, zeros,
+        // and numbers larger than n by 1s.
+        // After this convertion nums will contain 
+        // only positive numbers.
+        for (int i = 0; i < n; i++)
+            if ((nums[i] <= 0) || (nums[i] > n))
+                nums[i] = 1;
+
+        // Use index as a hash key and number sign as a presence detector.
+        // For example, if nums[1] is negative that means that number `1`
+        // is present in the array. 
+        // If nums[2] is positive - number 2 is missing.
+        for (int i = 0; i < n; i++) {
+            int a = Math.abs(nums[i]);
+            // If you meet number a in the array - change the sign of a-th element.
+            // Be careful with duplicates : do it only once.
+            if (a == n)
+                nums[0] = - Math.abs(nums[0]);
+            else
+                nums[a] = - Math.abs(nums[a]);
+        }
+
+        // Now the index of the first positive number 
+        // is equal to first missing positive.
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0)
+                return i;
+        }
+
+        if (nums[0] > 0)
+            return n;
+
+        return n + 1;
+    }
+}
