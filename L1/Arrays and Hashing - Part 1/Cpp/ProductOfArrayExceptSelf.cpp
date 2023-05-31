0a90c6a3cdfe27b05325c26c49c01e911cd99137
Riya Bansal <35702912+riyabansal98@users.noreply.github.com>
2023-06-01 01:02:29 +0530

    Update ProductOfArrayExceptSelf.cpp

--- L1/Arrays and Hashing - Part 1/Cpp/ProductOfArrayExceptSelf.cpp
+++ L1/Arrays and Hashing - Part 1/Cpp/ProductOfArrayExceptSelf.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> productExceptSelf(std::vector<int>& nums) {
+        int length = nums.size();
+        std::vector<int> L(length, 1);
+        std::vector<int> R(length, 1);
+        std::vector<int> answer(length, 0);
+
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        for (int i = 0; i < length; i++) {
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
+    }
+};
