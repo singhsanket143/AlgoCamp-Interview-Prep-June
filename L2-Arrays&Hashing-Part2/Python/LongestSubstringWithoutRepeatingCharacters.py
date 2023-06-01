785f7cbf3b645d4c61e7c1f34a2d8c2260498711
Riya Bansal <35702912+riyabansal98@users.noreply.github.com>
2023-06-01 18:11:44 +0530

    Create LongestSubstringWithoutRepeatingCharacters.py

---
+++ L2-Arrays&Hashing-Part2/Python/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        # mp stores the current index of a character
+        mp = {}
+
+        i = 0
+        # try to extend the range [i, j]
+        for j in range(n):
+            if s[j] in mp:
+                i = max(mp[s[j]], i)
+
+            ans = max(ans, j - i + 1)
+            mp[s[j]] = j + 1
+
+        return ans
