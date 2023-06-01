ca9b98f505da674a6843ac0b8958e918eee824fa
Riya Bansal <35702912+riyabansal98@users.noreply.github.com>
2023-06-01 18:03:54 +0530

    Create GroupAnagrams.java

---
+++ L2-Arrays&Hashing-Part2/Python/GroupAnagrams.java
@@ -0,0 +1,6 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
