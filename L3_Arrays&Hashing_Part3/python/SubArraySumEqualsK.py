94ea9044d43205920ba6a984af8a59e39c183efb
Riya Bansal <35702912+riyabansal98@users.noreply.github.com>
2023-06-03 01:40:52 +0530

    Update SubArraySumEqualsK.py

--- L3_Arrays&Hashing_Part3/python/SubArraySumEqualsK.py
+++ L3_Arrays&Hashing_Part3/python/SubArraySumEqualsK.py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def subarraySum(self, nums, k):
+        count = 0
+        sum = 0
+        map = defaultdict(int)
+        map[0] = 1
+        
+        for num in nums:
+            sum += num
+            
+            if (sum - k) in map:
+                count += map[sum - k]
+            
+            map[sum] += 1
+        
+        return count
