b10a55fa1e7003cad79e244991211c9c56499d6b
Riya Bansal <35702912+riyabansal98@users.noreply.github.com>
2023-06-29 02:16:27 +0530

    Create NextPermutation.py

---
+++ L5_TwoPointers_Part2/python/NextPermutation.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i + 1] <= nums[i]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        self.reverse(nums, i + 1)
+
+    def reverse(self, nums, start):
+        i, j = start, len(nums) - 1
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        
