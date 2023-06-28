271d20f2251f268d4e288cee19db39572608f36b
Riya Bansal <35702912+riyabansal98@users.noreply.github.com>
2023-06-29 02:11:02 +0530

    Create KDiffPairs.py

---
+++ L5_TwoPointers_Part2/python/KDiffPairs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+
+        nums = sorted(nums)
+
+        left = 0
+        right = 1
+
+        result = 0
+
+        while (left < len(nums) and right < len(nums)):
+            if (left == right or nums[right] - nums[left] < k):
+                # List item 1 in the text
+                right += 1
+            elif nums[right] - nums[left] > k:
+                # List item 2 in the text
+                left += 1
+            else:
+                # List item 3 in the text
+                left += 1
+                result += 1
+                while (left < len(nums) and nums[left] == nums[left - 1]):
+                    left += 1
+
+        return result
