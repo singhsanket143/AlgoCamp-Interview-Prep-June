4f549145c297571da87d5048717fd9583a205b8b
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-08-30 00:32:40 +0530

    Create ReorderList.py

---
+++ Linked_List1/Python/ReorderList.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        if not head:
+            return 
+        
+        # find the middle of linked list [Problem 876]
+        # in 1->2->3->4->5->6 find 4 
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+            
+        # reverse the second part of the list [Problem 206]
+        # convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
+        # reverse the second half in-place
+        prev, curr = None, slow
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next       
+
+        # merge two sorted linked lists [Problem 21]
+        # merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
+        first, second = head, prev
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
