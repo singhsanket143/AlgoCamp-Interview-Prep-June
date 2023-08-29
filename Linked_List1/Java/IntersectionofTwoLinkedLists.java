1dc3e6ff71265dce6bc15d10ad927b85c64b1e52
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-08-30 00:29:46 +0530

    Create IntersectionofTwoLinkedLists.java

---
+++ Linked_List1/Java/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        while (headA != null) {
+            ListNode pB = headB;
+            while (pB != null) {
+                if (headA == pB) return headA;
+                pB = pB.next;
+            }
+            headA = headA.next;
+        }
+        return null;
+    }
+}
