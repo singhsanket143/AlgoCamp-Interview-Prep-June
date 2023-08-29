d9fcc2c7771a55349272f0b9eb8901cf7580f22f
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-08-30 00:17:01 +0530

    Create ReverseLinkedList.py

---
+++ Linked_List1/Python/ReverseLinkedList.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if (not head) or (not head.next):
+            return head
+        
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
