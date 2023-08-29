3e29ad15a6de5035b6ecf9270e83dc4599fa778c
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-08-30 00:50:44 +0530

    Create RemoveNthNodeFromEndofList.py

---
+++ LinkedList2/python/RemoveNthNodeFromEndofList.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        def index(node):
+            if not node:
+                return 0
+            i = index(node.next) + 1
+            if i > n:
+                node.next.val = node.val
+            return i
+        index(head)
+        return head.next
