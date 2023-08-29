b6157b489f146029c23ed0c14aa011fd9168cd64
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-08-30 00:41:16 +0530

    Create LinkedListCycle.py

---
+++ LinkedList2/python/LinkedListCycle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
