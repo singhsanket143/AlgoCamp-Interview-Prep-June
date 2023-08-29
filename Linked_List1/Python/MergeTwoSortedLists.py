48e24cc73cf98d5dc4e6a7ed742bb19e18ff7b4f
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-08-30 00:26:53 +0530

    Create MergeTwoSortedLists.py

---
+++ Linked_List1/Python/MergeTwoSortedLists.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
