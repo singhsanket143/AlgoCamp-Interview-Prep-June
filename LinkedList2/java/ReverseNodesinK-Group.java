ad2a2207d9f7eee3c49e322f778984ca4c4fbc03
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-08-30 00:47:34 +0530

    Create ReverseNodesinK-Group.java

---
+++ LinkedList2/java/ReverseNodesinK-Group.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public ListNode reverseLinkedList(ListNode head, int k) {
+        
+        // Reverse k nodes of the given linked list.
+        // This function assumes that the list contains 
+        // atleast k nodes.
+        ListNode new_head = null;
+        ListNode ptr = head;
+        
+        while (k > 0) {
+            
+            // Keep track of the next node to process in the
+            // original list
+            ListNode next_node = ptr.next;
+            
+            // Insert the node pointed to by "ptr"
+            // at the beginning of the reversed list
+            ptr.next = new_head;
+            new_head = ptr;
+            
+            // Move on to the next node
+            ptr = next_node;
+            
+            // Decrement the count of nodes to be reversed by 1
+            k--;
+        }
+            
+            
+        // Return the head of the reversed list
+        return new_head;
+    
+    }
+            
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+        int count = 0;
+        ListNode ptr = head;
+        
+        // First, see if there are atleast k nodes
+        // left in the linked list.
+        while (count < k && ptr != null) {
+            ptr = ptr.next;
+            count++;
+        }
+            
+        
+        // If we have k nodes, then we reverse them
+        if (count == k) {
+            
+            // Reverse the first k nodes of the list and
+            // get the reversed list's head.
+            ListNode reversedHead = this.reverseLinkedList(head, k);
+            
+            // Now recurse on the remaining linked list. Since
+            // our recursion returns the head of the overall processed
+            // list, we use that and the "original" head of the "k" nodes
+            // to re-wire the connections.
+            head.next = this.reverseKGroup(ptr, k);
+            return reversedHead;
+        }
+            
+        return head;
+    }
+}
