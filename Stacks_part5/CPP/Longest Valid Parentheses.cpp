4363dda199fbae79708aaa5303333d2789dc67ad
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-09-03 18:06:28 +0530

    Create Longest Valid Parentheses.cpp

---
+++ Stacks_part5/CPP/Longest Valid Parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int maxL=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int t=stk.top();
+            if(t!=-1&&s[i]==')'&&s[t]=='(')
+            {
+                stk.pop();
+                maxL=max(maxL,i-stk.top());
+            }
+            else
+                stk.push(i);
+        }
+        return maxL;
+    }
+};
