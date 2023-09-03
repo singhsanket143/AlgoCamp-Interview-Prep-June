95e06bf6d098d8c2c9292b8f6488942a5b803cbf
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-09-03 19:05:08 +0530

    Create Min Stack.cpp

---
+++ Stacks_part5/CPP/Min Stack.cpp
@@ -0,0 +1,20 @@
+class MinStack {
+private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int x) {
+	    s1.push(x);
+	    if (s2.empty() || x <= getMin())  s2.push(x);	    
+    }
+    void pop() {
+	    if (s1.top() == getMin())  s2.pop();
+	    s1.pop();
+    }
+    int top() {
+	    return s1.top();
+    }
+    int getMin() {
+	    return s2.top();
+    }
+};
