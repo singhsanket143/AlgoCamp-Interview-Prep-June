e10e1574899938bd55ed3f9056cbccdd52336ca0
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-09-03 15:47:24 +0530

    Create Maximal Rectangle.java

---
+++ Stacks_Part3/java/Maximal Rectangle.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+
+        if (matrix.length == 0) return 0;
+        int maxarea = 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if (matrix[i][j] == '1'){
+
+                    // compute the maximum width and update dp with it
+                    dp[i][j] = j == 0? 1 : dp[i][j-1] + 1;
+
+                    int width = dp[i][j];
+
+                    // compute the maximum area rectangle with a lower right corner at [i, j]
+                    for(int k = i; k >= 0; k--){
+                        width = Math.min(width, dp[k][j]);
+                        maxarea = Math.max(maxarea, width * (i - k + 1));
+                    }
+                }
+            }
+        } return maxarea;
+    }
+}
