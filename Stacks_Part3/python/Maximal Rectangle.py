7e79bac32e61b348a3546eed19687271003023fe
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-09-03 15:48:09 +0530

    Create Maximal Rectangle.py

---
+++ Stacks_Part3/python/Maximal Rectangle.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        maxarea = 0
+
+        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == '0': continue
+
+                # compute the maximum width and update dp with it
+                width = dp[i][j] = dp[i][j-1] + 1 if j else 1
+
+                # compute the maximum area rectangle with a lower right corner at [i, j]
+                for k in range(i, -1, -1):
+                    width = min(width, dp[k][j])
+                    maxarea = max(maxarea, width * (i-k+1))
+        return maxarea
