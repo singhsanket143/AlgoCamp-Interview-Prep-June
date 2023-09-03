8fa12136afb60de5c3596cc568e1b295668d246e
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-09-03 18:05:06 +0530

    Create Longest Valid Parentheses.java

---
+++ Stacks_part5/Java/Longest Valid Parentheses.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int longestValidParentheses(String s) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        int result = 0;
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')' && stack.size() > 1 && s.charAt(stack.peek()) == '(') {
+                stack.pop();
+                result = Math.max(result, i - stack.peek());
+            } else {
+                stack.push(i);
+            }
+        }
+        return result;
+    }
+}
