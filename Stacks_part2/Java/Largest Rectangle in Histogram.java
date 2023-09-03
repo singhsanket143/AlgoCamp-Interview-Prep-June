5622f424ef9cba2ebcc85b72dc80b390783658cd
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-09-03 15:38:39 +0530

    Create  Largest Rectangle in Histogram.java

---
+++ Stacks_part2/Java/Largest Rectangle in Histogram.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        int length = heights.length;
+        int maxArea = 0;
+        for (int i = 0; i < length; i++) {
+            while ((stack.peek() != -1)
+                    && (heights[stack.peek()] >= heights[i])) {
+                int currentHeight = heights[stack.pop()];
+                int currentWidth = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, currentHeight * currentWidth);
+            }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            int currentHeight = heights[stack.pop()];
+            int currentWidth = length - stack.peek() - 1;
+            maxArea = Math.max(maxArea, currentHeight * currentWidth);
+        }
+        return maxArea;
+    }
+}
