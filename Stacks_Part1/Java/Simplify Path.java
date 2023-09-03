a7fefb7f11b1cd1c3d4604bf157a2df515ddcf1b
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-09-03 14:54:18 +0530

    Create Simplify Path.java

---
+++ Stacks_Part1/Java/Simplify Path.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String simplifyPath(String path) {
+
+        // Initialize a stack
+        Stack<String> stack = new Stack<String>();
+        String[] components = path.split("/");
+
+        // Split the input string on "/" as the delimiter
+        // and process each portion one by one
+        for (String directory : components) {
+
+            // A no-op for a "." or an empty string
+            if (directory.equals(".") || directory.isEmpty()) {
+                continue;
+            } else if (directory.equals("..")) {
+
+                // If the current component is a "..", then
+                // we pop an entry from the stack if it's non-empty
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+
+                // Finally, a legitimate directory name, so we add it
+                // to our stack
+                stack.add(directory);
+            }
+        }
+
+        // Stich together all the directory names together
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/");
+            result.append(dir);
+        }
+
+        return result.length() > 0 ? result.toString() : "/" ;
+    }
+}
