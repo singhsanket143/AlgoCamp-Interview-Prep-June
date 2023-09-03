17947ef3d70358f161c50ad49c0fe20569122a17
Sarthak Jain <41904293+sarthak-1998@users.noreply.github.com>
2023-09-03 18:07:14 +0530

    Create Longest Valid Parentheses.py

---
+++ Stacks_part5/Python/Longest Valid Parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = [0]
+        longest = 0
+        
+        for c in s:
+            if c == "(":
+                stack.append(0)
+            else:
+                if len(stack) > 1:
+                    val = stack.pop()
+                    stack[-1] += val + 2
+                    longest = max(longest, stack[-1])
+                else:
+                    stack = [0]
+
+        return longest
